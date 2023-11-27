739c8d01d587cd1eec1848df92b812de3d7ee79e
Zain kiani <zainkiani247@gmail.com>
2023-11-27 17:42:45 +0500

    popular picks done

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide,SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+import { json } from "react-router-dom";
 function Popular() {
 
   const [popular , setPopular] = useState([]);
@@ -11,9 +12,20 @@ function Popular() {
   },[])
 
   const getPopular = async () =>{
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+
+    const check = localStorage.getItem("popular");
+
+    if(check){
+      setPopular(JSON.parse(check));
+    }else{
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
     const data = await api.json();
+    localStorage.setItem("popular", JSON.stringify(data.recipes));
     setPopular(data.recipes);
+    console.log(data.recipes);
+    }
+
+    
   }
 
 
@@ -37,16 +49,18 @@ function Popular() {
             >
             {popular.map((recipe)=>{
               return(
-                <SplideSlide>
+                <SplideSlide key={recipe.id}>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient/>
                 </Card>
                 </SplideSlide>
               );
             })}
             </Splide>
           </Wrapper>
+          
     </div>
     
   )
@@ -61,6 +75,7 @@ const Card = styled.div`
   border-radius: 2rem;
   overflow: hidden;
   position: relative;
+  
 
 
   img{
@@ -86,7 +101,17 @@ const Card = styled.div`
     justify-content: center;
     align-items:center;
   }
+`;
+
+const Gradient = styled.div`
+      z-index: 3;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.7));
+
 `
+
 
 
 
